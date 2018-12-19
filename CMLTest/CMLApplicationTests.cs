b7717c784657e4d0efb9613105ba7bfd513ab990
DXTsT <2237367067@qq.com>
2018-12-20 07:25:48 +0800

    添加单元测试框架

---
+++ CMLTest/CMLApplicationTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CMLMusicPlayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMLMusicPlayer.Tests
+{
+	[TestClass()]
+	public class CMLApplicationTests
+	{
+		[TestMethod()]
+		public void InitTest()
+		{
+			CMLApplication application = new CMLApplication();
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void CMLApplicationTest1()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void RunTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
---
+++ CMLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CMLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CMLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ce6185f-1c88-4b89-a9ec-b37b898efef9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
