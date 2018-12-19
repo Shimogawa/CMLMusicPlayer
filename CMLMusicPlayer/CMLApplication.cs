88a5837ee61d5c29f04090125e17673090dab8cb
DXTsT <2237367067@qq.com>
2018-12-20 07:15:53 +0800

    修正刷新时间

--- CMLMusicPlayer/CMLApplication.cs
+++ CMLMusicPlayer/CMLApplication.cs
@@ -8,8 +8,11 @@ namespace CMLMusicPlayer
 {
 	public class CMLApplication
 	{
-		private int currentTime;
+		private long currentTime;
 		private double deltaTime;
+		private Random random;
+		private int count;
+
 		private readonly int frameRate;
 
 		private const int DEFAULT_FRAMERATE = 60;
@@ -17,30 +20,48 @@ namespace CMLMusicPlayer
 		public CMLApplication()
 		{
 			this.frameRate = DEFAULT_FRAMERATE;
+			init();
 		}
 
 		// Program用来处理程序参数，之后将所需参数传递到CMLApplication里面执行
 		public CMLApplication(int frameRate)
 		{
 			this.frameRate = frameRate;
+			init();
 		}
 
+		private void init()
+		{
+			count = 0;
+			random = new Random();
+		}
+		private void test()
+		{
+			int width = Console.WindowWidth;
+			int height = Console.WindowHeight;
+			int x = random.Next(width);
+			int y = random.Next(height);
+			Console.SetCursorPosition(x, y);
+			Console.Write(count++);
+		}
 
 		public void Run()
 		{
-			currentTime = DateTime.Now.Millisecond;
+			currentTime = DateTime.Now.Ticks;
 			while (true)
 			{
 				// 主要绘制函数区域
 
+				test();
 
 
-				int delta = DateTime.Now.Millisecond - currentTime;
+				long delta;
 
 				// [1s / frameRate] refresh duration 
-				while (delta < (1000.0 / frameRate))
+				while ((delta = DateTime.Now.Ticks - currentTime) < (1e7 / (double)frameRate))
 					;
-				deltaTime = delta / 1000.0;
+				deltaTime = delta / 1e7;
+				currentTime = DateTime.Now.Ticks;
 			}
 		}
 	}
--- CMLMusicPlayer/Program.cs
+++ CMLMusicPlayer/Program.cs
@@ -19,130 +19,133 @@ namespace CMLMusicPlayer
 
         static void Main(string[] args)
         {
-            if (args.Length == 0)
-            {
-                args = new [] { "-h" };
-                //args = new []
-                //{
-                //    "-p", src
-                //};
-            }
+			CMLApplication application = new CMLApplication();
+			application.Run();
+			if (args.Length == 0)
+			{
+				args = new[] { "-h" };
+				args = new[]
+				{
+					"-p", src
+				};
+			}
 
 #if DEBUG
-            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("zh-CN");
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("zh-CN");
 #endif
 
-            if (!ProcessArgs(args))
-            {
-                return;
-            }
+			if (!ProcessArgs(args))
+			{
+				return;
+			}
 
-            var player = new MusicPlayer(src)
-            {
-                Version = ver,
-                FR = frameRate > 120 ? 120 : frameRate
-            };
-            player.Run();
+			var player = new MusicPlayer(src)
+			{
+				Version = ver,
+				FR = frameRate > 120 ? 120 : frameRate
+			};
+			player.Run();
 
-            //if (Process.GetProcessById())
-        }
+			if (Process.GetProcessById())
 
-        private static bool ProcessArgs(string[] args)
-        {
-            ver = typeof(Program).Assembly.GetName().Version;
+		}
 
-            var app = new CommandLineApplication(false)
-            {
-                Name = typeof(Program).Namespace,
-                FullName = typeof(Program).Namespace,
-                ShortVersionGetter = () => ver.ToString(2),
-                LongVersionGetter = () => ver.ToString(3)
-            };
+		private static bool ProcessArgs(string[] args)
+		{
+			ver = typeof(Program).Assembly.GetName().Version;
 
-            app.HelpOption("--help | -h");
-            app.VersionOption("-v | --version", app.ShortVersionGetter, app.LongVersionGetter);
+			var app = new CommandLineApplication(false)
+			{
+				Name = typeof(Program).Namespace,
+				FullName = typeof(Program).Namespace,
+				ShortVersionGetter = () => ver.ToString(2),
+				LongVersionGetter = () => ver.ToString(3)
+			};
 
-            var frameRateOpt = app.Option("-f | --frame", Strings.FrameRateOption, CommandOptionType.SingleValue);
+			app.HelpOption("--help | -h");
+			app.VersionOption("-v | --version", app.ShortVersionGetter, app.LongVersionGetter);
 
-            //var playOption = app.Option("-p | --play",
-            //    "Play all the musics in the specified music folder.", CommandOptionType.SingleValue);
+			var frameRateOpt = app.Option("-f | --frame", Strings.FrameRateOption, CommandOptionType.SingleValue);
 
-            var pathArg = app.Argument("path", "The path of the music folder");
+			var playOption = app.Option("-p | --play",
+				"Play all the musics in the specified music folder.", CommandOptionType.SingleValue);
 
-            frameRate = 60;
+			var pathArg = app.Argument("path", "The path of the music folder");
 
-            app.OnExecute(() =>
-            {
-                if (frameRateOpt.HasValue())
-                {
-                    try
-                    {
-                        frameRate = int.Parse(frameRateOpt.Value());
-                        if (frameRate <= 0 || frameRate > 300)
-                            throw new Exception();
-                    }
-                    catch (Exception)
-                    {
-                    }
-                }
-                
-                src = pathArg.Value;
-                //if (string.IsNullOrWhiteSpace(src))
-                //{
-                //    //Console.WriteLine(Strings.PathNotSpecified);
-                //    //src = Path.Combine(Directory.GetCurrentDirectory(), "Musics");
-                //    //Directory.CreateDirectory(src);
-                //    return -1;
-                //}
-                //else if (src.Length > 2 && src[1] == ':')
-                //{
-                //    if (!Directory.Exists(src))
-                //    {
-                //        Console.WriteLine(Strings.WrongPath);
-                //        src = Path.Combine(Directory.GetCurrentDirectory(), "Musics");
-                //        Directory.CreateDirectory(src);
-                //    }
-                //}
-                //else
-                //{
-                //    src = Path.Combine(Directory.GetCurrentDirectory(), src);
-                //    if (!Directory.Exists(src))
-                //    {
-                //        Console.WriteLine(Strings.PathNotFound);
-                //        Directory.CreateDirectory(src);
-                //    }
-                //}
+			frameRate = 60;
 
-                return 0;
-            });
+			app.OnExecute(() =>
+			{
+				if (frameRateOpt.HasValue())
+				{
+					try
+					{
+						frameRate = int.Parse(frameRateOpt.Value());
+						if (frameRate <= 0 || frameRate > 300)
+							throw new Exception();
+					}
+					catch (Exception)
+					{
+					}
+				}
 
-            app.Execute(args);
+				src = pathArg.Value;
+				if (string.IsNullOrWhiteSpace(src))
+				{
+					//Console.WriteLine(Strings.PathNotSpecified);
+					//src = Path.Combine(Directory.GetCurrentDirectory(), "Musics");
+					//Directory.CreateDirectory(src);
+					return -1;
+				}
+				else if (src.Length > 2 && src[1] == ':')
+				{
+					if (!Directory.Exists(src))
+					{
+						Console.WriteLine(Strings.WrongPath);
+						src = Path.Combine(Directory.GetCurrentDirectory(), "Musics");
+						Directory.CreateDirectory(src);
+					}
+				}
+				else
+				{
+					src = Path.Combine(Directory.GetCurrentDirectory(), src);
+					if (!Directory.Exists(src))
+					{
+						Console.WriteLine(Strings.PathNotFound);
+						Directory.CreateDirectory(src);
+					}
+				}
 
-            if (string.IsNullOrWhiteSpace(src))
-            {
-                return false;
-            }
-            else if (src.Length > 2 && src[1] == ':')
-            {
-                if (!Directory.Exists(src))
-                {
-                    Console.WriteLine(Strings.WrongPath);
-                    src = Path.Combine(Directory.GetCurrentDirectory(), "Musics");
-                    Directory.CreateDirectory(src);
-                }
-            }
-            else
-            {
-                src = Path.Combine(Directory.GetCurrentDirectory(), src);
-                if (!Directory.Exists(src))
-                {
-                    Console.WriteLine(Strings.PathNotFound);
-                    Directory.CreateDirectory(src);
-                }
-            }
-            Thread.Sleep(1000);
+				return 0;
+			});
 
-            return true;
-        }
-    }
+			app.Execute(args);
+
+			if (string.IsNullOrWhiteSpace(src))
+			{
+				return false;
+			}
+			else if (src.Length > 2 && src[1] == ':')
+			{
+				if (!Directory.Exists(src))
+				{
+					Console.WriteLine(Strings.WrongPath);
+					src = Path.Combine(Directory.GetCurrentDirectory(), "Musics");
+					Directory.CreateDirectory(src);
+				}
+			}
+			else
+			{
+				src = Path.Combine(Directory.GetCurrentDirectory(), src);
+				if (!Directory.Exists(src))
+				{
+					Console.WriteLine(Strings.PathNotFound);
+					Directory.CreateDirectory(src);
+				}
+			}
+			Thread.Sleep(1000);
+
+			return true;
+		}
+	}
 }
